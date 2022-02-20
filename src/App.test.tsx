af5af030dcdd6bfa72130e4333923a7960d7a8bf
Marcos Andre <marcosdev.me@gmail.com>
2022-02-20 12:45:21 -0300

    test with RTL

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
+
 describe("testing math functions", () => {
   it("should add 1 to 2 and return 3", () => {
     const result = 1 + 2;
@@ -10,6 +14,18 @@ it("should multiply 5 by 3 and return 15", () => {
   const result = 5 * 3;
 
   expect(result).toBe(15);
+});
+
+it("should have a container div", () => {
+  const { container } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(container.getElementsByClassName("container").length).toBeGreaterThan(
+    0
+  );
 });
 
 export {};
