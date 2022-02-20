7febd71fb1b27be3f083b5864990c4744fba5916
Marcos Andre <marcosdev.me@gmail.com>
2022-02-20 13:55:13 -0300

    test with RTL

--- src/pages/CarList/index.test.tsx
+++ src/pages/CarList/index.test.tsx
@@ -1,26 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { CarList } from './index';
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { CarList } from ".";
 
-it('should have a list of cars', () => {
-    const { container } = render(
-        <MemoryRouter>
-            <CarList />
-        </MemoryRouter>
-    );
-    const listEl = container.getElementsByClassName('list');
-    expect(listEl.length).toBeGreaterThan(0);
+it("should have a list of cars", () => {
+  const { container } = render(
+    <MemoryRouter>
+      <CarList />
+    </MemoryRouter>
+  );
+
+  expect(container.getElementsByClassName("list").length).toBeGreaterThan(0);
 });
 
-it('should have 5 cars in the list', () => {
-    const { container } = render(
-        <MemoryRouter>
-            <CarList />
-        </MemoryRouter>
-    );
+it("should have 5 cars in the list", () => {
+  const { container } = render(
+    <MemoryRouter>
+      <CarList />
+    </MemoryRouter>
+  );
 
-    const carsList = container.getElementsByClassName('carItem');
-    expect(carsList.length).toEqual(5);
+  const carsList = container.getElementsByClassName("carItem");
+  expect(carsList.length).toBe(5);
 });
 
 export {};
