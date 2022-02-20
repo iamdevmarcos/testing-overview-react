2d3338970aa0c817bfeddf46462045ea07d1fd7e
Marcos Andre <marcosdev.me@gmail.com>
2022-02-20 12:36:41 -0300

    testing with RTL

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,16 +1,16 @@
-import { Header } from './index';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { Header } from ".";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 
 it('should have a "Cadastrar" button', () => {
-    render(
-        <MemoryRouter>
-            <Header />
-        </MemoryRouter>
-    );
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
 
-    let button = screen.getByText('Cadastrar');
-    expect(button).toBeInTheDocument();
+  const button = screen.getByText("Cadastrar");
+  expect(button).toBeInTheDocument();
 });
 
 export {};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,20 @@
-import './styles.css';
-import { Logo } from '../Logo';
-import { Link } from 'react-router-dom';
+import "./styles.css";
+import { Logo } from "../Logo";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header>
-            <div className="headerContainer">
-                <div className="headerLeft">
-                    <Logo />
-                </div>
-                <div className="headerRight">
-                    <Link to={'/car'} className="button">Cadastrar</Link>
-                </div>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header>
+      <div className="headerContainer">
+        <div className="headerLeft">
+          <Logo />
+        </div>
+        <div className="headerRight">
+          <Link to={"/car"} className="button">
+            Cadastrar
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
